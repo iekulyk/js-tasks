daf2291269646dc8a526a1023e344afc06a6258f
Evgeniy Kulyk <kulyk.evgeniy@gmail.com>
2016-06-20 22:53:05 +0300

    basic task with jquery

--- jquery/src/js/app.js
+++ jquery/src/js/app.js
@@ -1,4 +1,4 @@
-var TasksBoard = (function ($, doc, storageProvider, validator) {
+var TasksBoard = (function ($, doc, storage, validator) {
     var TasksBoard = {};
     var self = TasksBoard;
 
@@ -7,36 +7,42 @@ var TasksBoard = (function ($, doc, storageProvider, validator) {
         inputSelector: ''
     }
 
+    var $taskInput, $tasksList;
+
     self.init = function (options) {
-        _validateInitParams(options)
-        if (_validateInitParams(options)) {
-            $(options.inputSelector).keypress(self.addTask);
-            $(options.listSelector).on('click', '[data-action="remove-task"]', self.removeTaskHandler)
-        }
+        $taskInput = $(options.inputSelector);
+        $tasksList = $(options.listSelector);
+
+        $(options.inputSelector).keypress(self.addTaskHandler);
+        $(options.listSelector).on('click', '[data-action="remove-task"]', self.removeTaskHandler)
     }
 
     self.addTaskHandler = function (e) {
-        if (e.keyCode == 13) {
-            self.addTask($(this).val())
+        if (e.keyCode == 13 && _validateInputText()) {
+            var text = $taskInput.val();
+            var taskTemplate = '<li class="list-group-item"><span data-action="remove-task" class="badge">X</span>' + text + '</li>';
+            $tasksList.append(taskTemplate);
+            $taskInput.val('');
         }
     }
 
     self.removeTaskHandler = function (e) {
-
+        e.currentTarget.parentElement.remove();
     }
 
-    self.addTask = function (text) {
+    function _validateInputText() {
+        $taskInput.toggleClass('input-error', false);
+        $taskInput.attr('title', '');
+        if ($taskInput.val().length == 0) {
+            $taskInput.toggleClass('input-error', true);
+            $taskInput.attr('title', '* Please Fill Out This Field.');
+            $taskInput.tooltip('show');
+            return false
+        }
 
+        $taskInput.tooltip('destroy');
+        return true;
     }
-
-    self.removeTask = function (taskId) {
-
-    }
-
-    _validateInitParams = function () {
-
-    }
-
 
     return TasksBoard;
 })($, document, localStorage)

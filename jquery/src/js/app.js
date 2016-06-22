1d0bd409e86b42ffee1c7572c105bdb6c85042e2
ikulyk360 <ikulyk@360incentives.com>
2016-06-22 15:12:36 +0300

    jquery:task-module transformed to class

--- jquery/src/js/app.js
+++ jquery/src/js/app.js
@@ -1,43 +1,54 @@
 var TasksBoard = (function ($) {
-    var TasksBoard = {};
-    var self = TasksBoard;
+    TasksBoard = function (options) {
+        this.controls = {
+            $rootEl: $(),
+            $taskInput: $(),
+            $tasksList: $()
+        };
 
-    var $taskInput, $tasksList;
+        this.options = options;
+        this._init();
+    };
 
-    self.init = function (options) {
-        $taskInput = $(options.inputSelector);
-        $tasksList = $(options.listSelector);
+    TasksBoard.prototype = {
+        _init: function () {
+            var self = this;
 
-        $(options.inputSelector).keypress(self.addTaskHandler);
-        $(options.listSelector).on('click', '[data-action="remove-task"]', self.removeTaskHandler)
-    }
+            self.controls.$rootEl = $(self.options.rootSelector);
+            self.controls.$taskInput = self.controls.$rootEl.find(self.options.inputSelector);
+            self.controls.$tasksList = self.controls.$rootEl.find(self.options.listSelector);
 
-    self.addTaskHandler = function (e) {
-        if (e.keyCode == 13 && _validateInputText()) {
-            var text = $taskInput.val();
-            var taskTemplate = '<li class="list-group-item"><span data-action="remove-task" class="badge">X</span>' + text + '</li>';
-            $tasksList.append(taskTemplate);
-            $taskInput.val('');
+            self.controls.$taskInput.keypress(self._addTaskHandler.bind(self));
+            self.controls.$tasksList.on('click', '[data-action="remove-task"]', self._removeTaskHandler.bind(self));
+        },
+        _addTaskHandler: function (e) {
+            var self = this;
+            if (e.keyCode == 13 && self._validateInputText()) {
+                var text = self.controls.$taskInput.val();
+                var taskTemplate = '<li class="list-group-item"><span data-action="remove-task" class="badge">X</span>' + text + '</li>';
+                self.controls.$tasksList.append(taskTemplate);
+                self.controls.$taskInput.val('');
+            }
+        },
+        _removeTaskHandler: function (e) {
+            e.currentTarget.parentElement.remove();
+        },
+        _validateInputText: function () {
+            var self = this;
+            self.controls.$taskInput.toggleClass('input-error', false);
+            self.controls.$taskInput.attr('title', '');
+            if (self.controls.$taskInput.val().length == 0) {
+                self.controls.$taskInput.toggleClass('input-error', true);
+                self.controls.$taskInput.attr('title', '* Please Fill Out This Field.');
+                self.controls.$taskInput.tooltip('show');
+                return false
+            }
+
+            self.controls.$taskInput.tooltip('destroy');
+            return true;
         }
-    }
 
-    self.removeTaskHandler = function (e) {
-        e.currentTarget.parentElement.remove();
-    }
-
-    function _validateInputText() {
-        $taskInput.toggleClass('input-error', false);
-        $taskInput.attr('title', '');
-        if ($taskInput.val().length == 0) {
-            $taskInput.toggleClass('input-error', true);
-            $taskInput.attr('title', '* Please Fill Out This Field.');
-            $taskInput.tooltip('show');
-            return false
-        }
-
-        $taskInput.tooltip('destroy');
-        return true;
-    }
+    };
 
     return TasksBoard;
-})(this.$)
+})($);
